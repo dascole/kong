d107a8670aaeb927234b6b2a07c9af6500f1c045
Joe Dascole <joe.dascole@konghq.com>
2023-09-19 09:50:57 -0400

    feat(cli): add redis connectivity test
    Add the ability to test connectivity to redis instances directly through
    the lua-resty-redis library.

--- kong/cmd/trouble.lua
+++ kong/cmd/trouble.lua
@@ -1,4 +1,6 @@
 local log = require "kong.cmd.utils.log"
+local redis_utils = require "kong.cmd.utils.redis"
+local inspect = require("inspect")
 
 local socket = require "socket"
 
@@ -21,6 +23,10 @@ local function execute(args)
     return test_tcp(args[1], args[2])
   end
 
+  if args.command == "redis" then
+                    -- connect(host,    port,    password,opts)
+    return redis_utils.connect(args[1], args[2], args[3], arg[4], arg[5])
+  end
   error("unknown command '" .. args.command .. "'")
 end
 
@@ -30,7 +36,8 @@ Usage: kong trouble COMMAND [OPTIONS]
 Troubleshoot basic aspects of operating Kong.
 
 The available commands are:
-  tcp <host> <port>                   Connect to a tcp socket of your choosing.
+  tcp <host> <port>                                      Connect to a tcp socket of your choosing.
+  redis --host <host> <port> <password> <ssl> <verify>   Connect to a redis instance of your choosing.
 ]]
 
 return {
@@ -38,5 +45,6 @@ return {
   execute = execute,
   sub_commands = {
     tcp = true,
+    redis = true,
   },
 }
